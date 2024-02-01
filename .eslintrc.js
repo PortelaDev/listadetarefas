40617ba0512fe883f9a2f7689a9477746f8bce07
PortelaDev <caioportela88@gmail.com>
2024-02-01 09:03:18 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parser: 'babel-eslint',
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+      },
+  },
+  rules: {
+    "react/jsx-filename-extension": "off",
+    'react/state-in-constructor': "off",
+    "react/forbid-prop-types": "off",
+  },
+};
+
+/* 
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parser: 'babel-eslint',
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    "react/jsx-filename-extension": "off"
+  },
+};
+
+
+*/
---
+++ README.md
@@ -0,0 +1,4 @@
+# exercicio de uma Lista de Tarefas (todo list)
+
+Exercicio do curso de JS, criando um CRUD usando as tecnologias:
+React, JSx, Css.
